7d334158ddc1ebd02508d1c374406836c1f44b94
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-12 15:08:42 +0530

    Update c3-static-website.tf

--- terraform-manifests/c3-static-website.tf
+++ terraform-manifests/c3-static-website.tf
@@ -9,10 +9,10 @@ module "azure_static_website" {
   #version = "2.0.0"
 
   # Github Clone over HTTPS 
-  source = "github.com/stacksimplify/terraform-azurerm-staticwebsitepublic"
+  #source = "github.com/stacksimplify/terraform-azurerm-staticwebsitepublic"
 
   # Github Clone over SSH (if git SSH configured with your repo - https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh)
-  #source = "git@github.com:stacksimplify/terraform-azurerm-staticwebsitepublic.git"
+  source = "git@github.com:subbuj1210/terraform-azure-vm.git"
 
   # Generic GIT repo: Github HTTPS with selecting a Specific Release Tag
   #source = "git::https://github.com/stacksimplify/terraform-azurerm-staticwebsitepublic.git?ref=1.0.0"
