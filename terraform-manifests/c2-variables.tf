7acc7dd0f4c9c297ab69131137719920422d95c1
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-12 17:43:21 +0530

    Update c2-variables.tf

--- terraform-manifests/c2-variables.tf
+++ terraform-manifests/c2-variables.tf
@@ -4,8 +4,10 @@
 variable "location" {
   description = "The Azure Region in which all resources groups should be created."
   type = string 
+  location = "eastus"
 }
 variable "resource_group_name" {
   description = "The name of the resource group"
   type = string     
+  resource_group_name = "myrg1"
 }
