36dcc17a2007681ff5c4f7ab9893251cd4fd70f6
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-12 14:16:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "rg_location" {
 }
 
 module "app1-rg" {
-        source = "git@github.com:subbuj1210/Azure-VM.git"
+        source = "../rg"
         rg_name = var.rg_name
         rg_location = var.rg_location
 }
