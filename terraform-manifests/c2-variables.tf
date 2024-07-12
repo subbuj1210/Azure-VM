d7170066f3f43f365f0527a553b43e2e22b6eccf
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-12 17:49:43 +0530

    Update c2-variables.tf

--- terraform-manifests/c2-variables.tf
+++ terraform-manifests/c2-variables.tf
@@ -1,13 +1,3 @@
 # Input variable definitions
 ## Place holder file
 # Input variable definitions
-variable "location" {
-  description = "The Azure Region in which all resources groups should be created."
-  type = string 
-  location = "eastus"
-}
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type = string     
-  resource_group_name = "myrg1"
-}
