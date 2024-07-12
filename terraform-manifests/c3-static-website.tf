0c69dff791bb4f098c74532c39a7b3c9edeeb969
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-12 17:56:19 +0530

    Update c3-static-website.tf

--- terraform-manifests/c3-static-website.tf
+++ terraform-manifests/c3-static-website.tf
@@ -17,5 +17,16 @@ module "azure_static_website" {
   # Generic GIT repo: Github HTTPS with selecting a Specific Release Tag
   #source = "git::https://github.com/stacksimplify/terraform-azurerm-staticwebsitepublic.git?ref=1.0.0"
 
+# Resource Group
+  location = "eastus"
+  resource_group_name = "myrg1"
+
+  # Storage Account
+  storage_account_name = "staticwebsite"
+  storage_account_tier = "Standard"
+  storage_account_replication_type = "LRS"
+  storage_account_kind = "StorageV2"
+  static_website_index_document = "index.html"
+  static_website_error_404_document = "error.html"
  
 }
