dcb675128dadee691404c8e6c8a582939f976096
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-12 13:49:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Call our Custom Terraform Module which we built earlier
-module "azure_static_website" {  
+module "rg" {  
 # Github Clone over SSH (if git SSH configured with your repo - https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh)
 source = "git@github.com:subbuj1210/Azure-VM.git"
 # Resource Group
