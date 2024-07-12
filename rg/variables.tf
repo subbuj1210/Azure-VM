a9d9115c78dc4373c4592a1515fb0afc64bdf72b
subbuj1210 <137508963+subbuj1210@users.noreply.github.com>
2024-07-12 15:06:47 +0530

    Delete rg directory

--- rg/variables.tf
+++
@@ -1,9 +0,0 @@
-# Input variable definitions
-variable "location" {
-  description = "The Azure Region in which all resources groups should be created."
-  type = string 
-}
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type = string     
-}
